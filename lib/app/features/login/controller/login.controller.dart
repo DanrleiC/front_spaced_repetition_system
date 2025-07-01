7427ce4a44a574070f8b239ac247bf646467a9e6
Danrlei <danrlei0601@gmail.com>
2025-07-01 19:47:48 -0300

    Update login.controller.dart

--- lib/app/features/login/controller/login.controller.dart
+++ lib/app/features/login/controller/login.controller.dart
@@ -1,17 +1,39 @@
-// login_controller.dart
 import 'package:flutter/material.dart';
-import 'package:riverpod/riverpod.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:front_spaced_repetition_system/app/repository/provider/api_service.provider.dart';
 import '../../../utils/routes.dart';
 
-class LoginController extends StateNotifier<AsyncValue<void>> {
-  LoginController() : super(const AsyncData(null));
+final loginControllerProvider = StateNotifierProvider<LoginController, AsyncValue<void>>((ref) => LoginController(ref));
 
-  Future<void> login({required BuildContext context, required String email, required String password}) async {
+class LoginController extends StateNotifier<AsyncValue<void>> {
+  final Ref ref;
+
+  LoginController(this.ref) : super(const AsyncData(null));
+
+  Future<void> login({ required BuildContext context, required String email, required String password }) async {
     state = const AsyncLoading();
+
     try {
-      // await authRepository.login(email, password);
-      Navigator.pushNamed(context, AppRoutes.homepage);
+      final api = ref.read(apiServiceProvider);
+
+      final response = await api.post('/users/login', body: {
+        'email': email,
+        'password': password,
+      });
+
+      // Exemplo: recebendo o token
+      final token = response['token'];
+
+      if (token == null) throw Exception('Token não retornado');
+
+      // Se quiser salvar o token:
+      ref.read(authTokenProvider.notifier).state = token;
+
+      // Navegação para a home
+      if (context.mounted) {
+        Navigator.pushReplacementNamed(context, AppRoutes.homepage);
+      }
+
       state = const AsyncData(null);
     } catch (e, st) {
       state = AsyncError(e, st);
