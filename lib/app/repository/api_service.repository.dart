a423b673112a07b6e4f79aac925e3bb1b390a4ec
Danrlei <danrlei0601@gmail.com>
2025-07-19 21:01:13 -0300

    Update api_service.repository.dart

--- lib/app/repository/api_service.repository.dart
+++ lib/app/repository/api_service.repository.dart
@@ -1,72 +1,95 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-
+import 'package:dio/dio.dart';
 import 'api_service_exeption.dart';
 
 class ApiService {
   final String baseUrl;
   final Map<String, String> defaultHeaders;
+  final Dio _dio;
 
   ApiService({
     this.baseUrl = "https://back-spaced-repetition-system.onrender.com",
     this.defaultHeaders = const {},
-  });
+  }) : _dio = Dio(BaseOptions(
+          baseUrl: baseUrl,
+          headers: {
+            'Content-Type': 'application/json',
+            ...defaultHeaders,
+          },
+        ));
 
-  Map<String, String> _buildHeaders([Map<String, String>? headers]) {
-    return {
-      'Content-Type': 'application/json',
-      ...defaultHeaders,
-      if (headers != null) ...headers,
-    };
-  }
-
-  Future<dynamic> get(String endpoint, { Map<String, String>? headers, Map<String, dynamic>? queryParameters }) async {
-    final uri = Uri.parse('$baseUrl$endpoint').replace(queryParameters: queryParameters);
-    final response = await http.get(uri, headers: _buildHeaders(headers));
-    return _handleResponse(response);
-  }
-
-  Future<dynamic> post(String endpoint, { Map<String, String>? headers, dynamic body }) async {
-    final uri = Uri.parse('$baseUrl$endpoint');
-    final response = await http.post(
-      uri,
-      headers: _buildHeaders(headers),
-      body: jsonEncode(body),
-    );
-    return _handleResponse(response);
-  }
-
-  Future<dynamic> put( String endpoint, { Map<String, String>? headers, dynamic body }) async {
-    final uri = Uri.parse('$baseUrl$endpoint');
-    final response = await http.put(
-      uri,
-      headers: _buildHeaders(headers),
-      body: jsonEncode(body),
-    );
-    return _handleResponse(response);
-  }
-
-  Future<dynamic> delete(String endpoint, { Map<String, String>? headers, dynamic body }) async {
-    final uri = Uri.parse('$baseUrl$endpoint');
-    final response = await http.delete(
-      uri,
-      headers: _buildHeaders(headers),
-      body: jsonEncode(body),
-    );
-    return _handleResponse(response);
-  }
-
-  dynamic _handleResponse(http.Response response) {
-    final statusCode = response.statusCode;
-    final body = response.body;
-
-    if (statusCode >= 200 && statusCode < 300) {
-      return jsonDecode(body);
-    } else {
-      throw HttpException(
-        statusCode: statusCode,
-        message: body,
+  Future<dynamic> get(
+    String endpoint, {
+    Map<String, String>? headers,
+    Map<String, dynamic>? queryParameters,
+  }) async {
+    try {
+      final response = await _dio.get(
+        endpoint,
+        queryParameters: queryParameters,
+        options: Options(headers: headers),
       );
+      return response.data;
+    } on DioException catch (e) {
+      _handleDioError(e);
     }
   }
+
+  Future<dynamic> post(
+    String endpoint, {
+    Map<String, String>? headers,
+    dynamic body,
+  }) async {
+    try {
+      final response = await _dio.post(
+        endpoint,
+        data: body,
+        options: Options(headers: headers),
+      );
+      return response.data;
+    } on DioException catch (e) {
+      _handleDioError(e);
+    }
+  }
+
+  Future<dynamic> put(
+    String endpoint, {
+    Map<String, String>? headers,
+    dynamic body,
+  }) async {
+    try {
+      final response = await _dio.put(
+        endpoint,
+        data: body,
+        options: Options(headers: headers),
+      );
+      return response.data;
+    } on DioException catch (e) {
+      _handleDioError(e);
+    }
+  }
+
+  Future<dynamic> delete(
+    String endpoint, {
+    Map<String, String>? headers,
+    dynamic body,
+  }) async {
+    try {
+      final response = await _dio.delete(
+        endpoint,
+        data: body,
+        options: Options(headers: headers),
+      );
+      return response.data;
+    } on DioException catch (e) {
+      _handleDioError(e);
+    }
+  }
+
+  Never _handleDioError(DioException e) {
+    final response = e.response;
+    final statusCode = response?.statusCode ?? 500;
+    final message = response?.data.toString() ?? e.message;
+    throw HttpException(statusCode: statusCode, message: message.toString());
+  }
 }
+
