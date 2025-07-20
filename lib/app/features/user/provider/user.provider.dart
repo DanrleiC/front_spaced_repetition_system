0d5e37f0a17765603ecbd827ca9a571fb89dbfb4
Danrlei <danrlei0601@gmail.com>
2025-07-19 21:01:04 -0300

    Delete user.provider.dart

--- lib/app/features/user/provider/user.provider.dart
+++
@@ -1,5 +0,0 @@
-// providers.dart
-import 'package:front_spaced_repetition_system/app/features/user/controller/user.controller.dart';
-import 'package:riverpod/riverpod.dart';
-
-final userControllerProvider = StateNotifierProvider<UserController, AsyncValue<void>>((ref) => UserController());
