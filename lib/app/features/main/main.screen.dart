5b27f371017cb971b87d78b9cd1c6a94226603d6
Danrlei <danrlei0601@gmail.com>
2025-07-19 21:00:34 -0300

    Create main.screen.dart

---
+++ lib/app/features/main/main.screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:front_spaced_repetition_system/app/features/homepage/provider/bottombar.provider.dart';
+import 'package:front_spaced_repetition_system/app/features/homepage/screen/homepage.screen.dart';
+import 'package:front_spaced_repetition_system/app/features/homepage/widgets/fab.widget.dart';
+import 'package:front_spaced_repetition_system/app/features/menu/screen/menu.screen.dart';
+import 'package:front_spaced_repetition_system/app/utils/colors_app.dart';
+
+class MainScreen extends ConsumerWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final selectedIndex = ref.watch(selectedIndexProvider);
+
+    // Lista das telas que serão exibidas
+    final List<Widget> screens = [
+      const HomePageScreen(),
+      const MenuScreen(),
+    ];
+
+    return Scaffold(
+      body: IndexedStack(
+        index: selectedIndex,
+        children: screens,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: ColorsApp.cardBackgound,
+        currentIndex: selectedIndex,
+        onTap: (index) => ref.read(selectedIndexProvider.notifier).state = index,
+        selectedItemColor: ColorsApp.mainPurple,
+        unselectedItemColor: ColorsApp.fieldBackground,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.house),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.bars),
+            label: 'Menu',
+          ),
+        ],
+      ),
+      floatingActionButton: const CustomFloatingActionButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
